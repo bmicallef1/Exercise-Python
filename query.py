53b47996a3dae90ab01f8538b1a9bc108e45dfbd
Brandon Micallef <145463013+bmicallef1@users.noreply.github.com>
2023-10-27 19:04:20 +0000

    feat: Created a menu
    Created a menu that loops until the user enters "q" and if the user enters an invalid option it allows the user to enter the option again.
    
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,15 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+userInput = " "
+while userInput != ("q"):
+    print("q: Quit")
+    
+    userInput = input("\nEnter option: ")
+    userInput = userInput.lower()
+
+    if userInput == "q":
+        break
+    else:
+        print("Invalid Option!\n")
