48e741ded2c970d160747312ebd086169265860c
Brandon Micallef <145463013+bmicallef1@users.noreply.github.com>
2023-10-27 19:36:24 +0000

    feat: List all movie names
    Added an option (list) where the user can request a list of all movie names on record. Movie cast is not diplayed. Each movie is displayed on one line and the number of the movie is also displayed.
    
    issue #3

--- query.py
+++ query.py
@@ -5,11 +5,18 @@ movies = Movies('./movies.txt')
 userInput = " "
 while userInput != ("q"):
     print("q: Quit")
+    print("list: Print all the Movie Names")
     
     userInput = input("\nEnter option: ")
     userInput = userInput.lower()
 
     if userInput == "q":
         break
+    elif userInput == "list":
+        print("\nList of all Movie Names: ")
+        totalMovies = len(movies._movies)
+        for movie in range(totalMovies):
+            print("#", movie, ": ", movies._movies[movie]['name'])
+        print()
     else:
         print("Invalid Option!\n")
